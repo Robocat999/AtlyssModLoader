fdcd3c5831a7509f8be04666cde6cdff0820729a
blued <blued@DYLANBEDROOMPC>
2024-02-22 01:58:22 -0800

    Add project files.

---
+++ AtlyssLoaderInjector/AtlyssModInjector.cs
@@ -0,0 +1,183 @@
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+// This script uses / adapts the BTMLInjector (Public Domain)
+// https://github.com/BattletechModders/BattleTechModLoader/releases
+namespace AtlyssLoaderInjector
+{
+    internal class AtlyssModInjector
+    {
+        private const string ModLoaderDllFileName = "AtlyssModLoader.dll";
+
+        private const string GameDllFileName = "Assembly-CSharp.dll";
+        private const string BackupFileExt = ".orig";
+
+        // TODO: These need to bedone for Atlyss
+        private const string HookType = "GameManager";
+        private const string HookMethod = "Awake";
+        private const string InjectType = "AtlyssModLoader.AtlyssModLoader";
+        private const string InjectMethod = "Init";
+
+        private static int Main(string[] args)
+        {
+            Console.WriteLine("Press any key to being the Atlyys Mod Injector");
+            Console.ReadKey();
+            Console.WriteLine("Started injector main");
+            var directory = Directory.GetCurrentDirectory();
+            var gameDllPath = Path.Combine(directory, "..", "ATLYSS_Data", "Managed", GameDllFileName);
+            gameDllPath = Path.GetFullPath(gameDllPath);
+            var gameDllBackupPath = Path.Combine(directory, "..", "ATLYSS_Data", "Managed", GameDllFileName + BackupFileExt);
+            gameDllBackupPath = Path.GetFullPath(gameDllBackupPath);
+            var modLoaderDllPath = Path.Combine(directory, "..", "ATLYSS_Data", "Managed", ModLoaderDllFileName);
+            modLoaderDllPath = Path.GetFullPath(modLoaderDllPath);
+
+            var injected = IsInjected(gameDllPath);
+            if (injected)
+            {
+                Console.WriteLine("Injector is already injected");
+                Console.ReadKey();
+                return 1;
+            }
+
+            Console.WriteLine("Attempting injection");
+            Backup(gameDllPath, gameDllBackupPath);
+            Inject(gameDllPath, modLoaderDllPath);
+
+            Console.WriteLine("The Atlyss Mod Injector has successfully completed!");
+            Console.ReadKey();
+            return 0;
+        }
+
+        private static void Backup(string filePath, string backupFilePath)
+        {
+            Console.WriteLine("Backing up orginal .dll");
+            File.Copy(filePath, backupFilePath, true);
+        }
+
+        private static void Inject(string hookFilePath, string injectFilePath)
+        {
+            Console.WriteLine("Injecting");
+            string oldDirectory = Directory.GetCurrentDirectory();
+            string newDirectory = Path.GetDirectoryName(hookFilePath);
+            Directory.SetCurrentDirectory(newDirectory);
+            Console.WriteLine(oldDirectory);
+            Console.WriteLine(newDirectory);
+            Console.WriteLine(hookFilePath);
+            Console.WriteLine(injectFilePath);
+            var success = true;
+            using (var game = ModuleDefinition.ReadModule(hookFilePath, new ReaderParameters { ReadWrite = true }))
+            using (var injecting = ModuleDefinition.ReadModule(injectFilePath))
+            {
+                success = InjectModHookPoint(game, injecting);
+                if (success)
+                {
+                    success = WriteNewAssembly(hookFilePath, game);
+                    if (!success)
+                        Console.WriteLine("WriteNewAssembly FAILED!");
+                } else
+                    Console.WriteLine("InjectModHookPoint FAILED!");
+            }
+            Directory.SetCurrentDirectory(oldDirectory);
+        }
+
+        // Primarily used to save modified assembly
+        private static bool WriteNewAssembly(string hookFilePath, ModuleDefinition game)
+        {
+            Console.WriteLine("Writing new assembly");
+            game.Write();
+            return true;
+        }
+
+        private static bool InjectModHookPoint(ModuleDefinition game, ModuleDefinition injecting)
+        {
+            Console.WriteLine("Injecting hookpoints");
+            // get the methods that we're hooking and injecting
+            var injectedMethod = injecting.GetType(InjectType).Methods.Single(x => x.Name == InjectMethod);
+            var hookedMethod = game.GetType(HookType).Methods.First(x => x.Name == HookMethod);
+
+            // If the return type is an iterator -- need to go searching for its MoveNext method which contains the actual code you'll want to inject
+            if (hookedMethod.ReturnType.Name.Equals("IEnumerator"))
+            {
+                var nestedIterator = game.GetType(HookType).NestedTypes.First(x => x.Name.Contains(HookMethod) && x.Name.Contains("Iterator"));
+                hookedMethod = nestedIterator.Methods.First(x => x.Name.Equals("MoveNext"));
+            }
+
+            // TODO: Evaluate if this is a thing Atlyss needs to deal with
+            // We want to inject after the PrepareSerializer call -- so search for that call in the CIL
+            int targetInstruction = -1;
+            for (int i = 0; i < hookedMethod.Body.Instructions.Count; i++)
+            {
+                var instruction = hookedMethod.Body.Instructions[i];
+                if (instruction.OpCode.Code.Equals(Code.Call) && instruction.OpCode.OperandType.Equals(OperandType.InlineMethod))
+                {
+                    MethodReference methodReference = (MethodReference)instruction.Operand;
+                    Console.WriteLine(methodReference.Name);
+                    if (methodReference.Name.Contains("DontDestroyOnLoad"))
+                    {
+                        targetInstruction = i;
+                    }
+                }
+            }
+
+            if (targetInstruction == -1)
+            {
+                Console.WriteLine("Target Instruction was left as -1, indicating a failuire to find DontDestroyOnLoad");
+                return false;
+            }
+                
+            hookedMethod.Body.GetILProcessor().InsertAfter(hookedMethod.Body.Instructions[targetInstruction],
+                Instruction.Create(OpCodes.Call, game.ImportReference(injectedMethod)));
+            return true;
+        }
+
+        private static bool IsInjected(string dllPath)
+        {
+            Console.WriteLine("Checks if .dll is injected");
+            var detectedInject = false;
+
+            using (var dll = ModuleDefinition.ReadModule(dllPath))
+            {
+                foreach (TypeDefinition type in dll.Types)
+                {
+                    // Standard methods
+                    foreach (var methodDefinition in type.Methods)
+                    {
+                        if (IsHookInstalled(methodDefinition))
+                        {
+                            detectedInject = true;
+                        }
+                    }
+
+                    // Also have to check in places like IEnumerator generated methods (Nested)
+                    foreach (var nestedType in type.NestedTypes)
+                    {
+                        foreach (var methodDefinition in nestedType.Methods)
+                        {
+                            if (IsHookInstalled(methodDefinition))
+                            {
+                                detectedInject = true;
+                            }
+                        }
+                    }
+                }
+            }
+            return detectedInject;
+        }
+
+        private static bool IsHookInstalled(MethodDefinition methodDefinition)
+        {
+            if (methodDefinition.Body == null)
+                return false;
+
+            foreach (var instruction in methodDefinition.Body.Instructions)
+            {
+                if (instruction.OpCode.Equals(OpCodes.Call) &&
+                    instruction.Operand.ToString().Equals($"System.Void {InjectType}::{InjectMethod}()"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ AtlyssModLoader/AtlyssModLoader.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using System.IO;
+
+
+// This script uses / adapts the BTMLModLoader (Public Domain)
+// https://github.com/BattletechModders/BattleTechModLoader/releases
+namespace AtlyssModLoader
+{
+    public static class AtlyssModLoader
+    {
+        private const BindingFlags PUBLIC_STATIC_BINDING_FLAGS = BindingFlags.Public | BindingFlags.Static;
+        private static readonly List<string> IGNORE_FILE_NAMES = new List<string>()
+        {
+            "0Harmony.dll",
+            "AtlyssModLoader.dll"
+        };
+
+        public static void LoadDLL(string path, string methodName = "Init", string typeName = null,
+            object[] prms = null, BindingFlags bFlags = PUBLIC_STATIC_BINDING_FLAGS)
+        {
+            FileLog.Log("AtlyssModLoader is attempting to laod a DLL");
+            var fileName = Path.GetFileName(path);
+            
+            try
+            {
+                var assembly = Assembly.LoadFrom(path);
+                var name = assembly.GetName();
+                var version = name.Version;
+                var types = new List<Type>();
+
+                // Find the type/s with our entry point/s
+                if (typeName == null)
+                {
+                    types.AddRange(assembly.GetTypes().Where(x => x.GetMethod(methodName, bFlags) != null));
+                }
+                else
+                {
+                    types.Add(assembly.GetType(typeName));
+                }
+
+                if (types.Count == 0)
+                {
+                    FileLog.Log("  DLL loader type count was 0");
+                    return;
+                }
+
+                foreach (var type in types)
+                {
+                    var entryMethod = type.GetMethod(methodName, bFlags);
+                    var methodParams = entryMethod?.GetParameters();
+
+                    if (methodParams == null)
+                        continue;
+
+                    if (methodParams.Length == 0)
+                    {
+                        FileLog.Log(  "AtlyssModLoader inserted a DLL method with no params");
+                        entryMethod.Invoke(null, null);
+                        continue;
+                    }
+
+                    // match up the passed in params with the method's params, if they match, call the method
+                    if (prms != null && methodParams.Length == prms.Length)
+                    {
+                        var paramsMatch = true;
+                        for (var i = 0; i < methodParams.Length; i++)
+                        {
+                            if (prms[i] != null && prms[i].GetType() != methodParams[i].ParameterType)
+                            {
+                                paramsMatch = false;
+                            }
+                        }
+
+                        if (paramsMatch)
+                        {
+                            FileLog.Log("  AtlyssModLoader inserted a DLL method with params");
+                            entryMethod.Invoke(null, prms);
+                            continue;
+                        }
+                    }
+
+                    FileLog.Log("  DLL loader fell through!!!!!");
+                }
+            }
+            catch (Exception e)
+            {
+                FileLog.Log("  DLL hit an unexpected error!");
+            }
+        }
+
+        public static void Init()
+        {
+            Harmony.DEBUG = true;
+            FileLog.Log("AtlyssModLoader Init has begun");
+            var loaderDirectory = Directory.GetCurrentDirectory();
+            var modDirectory = Path.GetFullPath(Path.Combine(loaderDirectory, "Mods"));
+            modDirectory = Path.GetFullPath(modDirectory);
+
+            if (!Directory.Exists(modDirectory))
+                Directory.CreateDirectory(modDirectory);
+
+            var harmony = new Harmony("io.github.robocat999.AtlyssModLoader");
+
+            FileLog.Log(modDirectory);
+            var dllPaths = Directory.GetFiles(modDirectory).Where(x => Path.GetExtension(x).ToLower() == ".dll").ToArray();
+
+            // Catch no mods loaded
+            if (dllPaths.Length == 0)
+            {
+                FileLog.Log("AtlyssModLoader Init found no mods to load");
+                return;
+            }
+
+            foreach ( var dllPath in dllPaths)
+            {
+                if (!IGNORE_FILE_NAMES.Contains(Path.GetFileName(dllPath)))
+                    LoadDLL(dllPath);
+            }
+            FileLog.Log("AtlyssModLoader Init has completed normally");
+        }
+
+
+    }
+}
