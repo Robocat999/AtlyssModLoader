bb1dc7f5b499fdc43755cef4b85db07f54eb9ffd
Robocat999 <blue.dylan9@gmail.com>
2024-09-13 00:40:57 -0700

    Syncing devlopment

--- AtlyssModLoader/obj/Debug/net461/AtlyssModLoader.AssemblyInfo.cs
+++ AtlyssModLoader/obj/Debug/net461/AtlyssModLoader.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtlyssModLoader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c36a9fd00b78409d7080aa2d035786592aacb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee9b90d4ec81bce0178f4d6640904f03ced267ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtlyssModLoader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtlyssModLoader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
